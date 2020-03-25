51ce8a52c93e821c29f2974af689bb9c4afdf0ce
Ilya Kasperovich <kasper.ii2108@gmail.com>
2020-03-25 15:03:42 +0300

    Updates

---
+++ precache-manifest.e6768201e4094add284dafc4c327b055.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf8fc866c662eb427929af3221dada4d",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "9ae39d78e53254018d65",
+    "url": "/weather-app/static/css/main.adb16eaf.chunk.css"
+  },
+  {
+    "revision": "98f2e82f54af7f51a66f",
+    "url": "/weather-app/static/js/2.36cb89e3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/weather-app/static/js/2.36cb89e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ae39d78e53254018d65",
+    "url": "/weather-app/static/js/main.b46c02c6.chunk.js"
+  },
+  {
+    "revision": "55676f7ecad5f9b6942e",
+    "url": "/weather-app/static/js/runtime-main.58bc7688.js"
+  }
+]);
